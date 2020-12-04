d67a98e8b215f65b2631c443df9c26e827305434
Steve Alverson <alverson.s@yahoo.com>
2020-12-04 18:35:19 -0500

    Added check form validity

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form');
+const password1El = document.getElementById('password1');
+const password2El = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm() {
+  // Using Constraint API
+  isValid = form.checkValidity();
+  // Style main message for error
+  message.textContent = 'Please fill out all fields!';
+  message.style.color = 'red';
+  message.style.borderColor = 'red';
+
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  // Validate Form
+  validateForm();
+}
+
+
+// Event Listeners
+form.addEventListener('submit', processFormData);
