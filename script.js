e5584ece0fb06b7505ea220c60a4da64bdac69f0
Steve Alverson <alverson.s@yahoo.com>
2020-12-04 19:00:38 -0500

    Passwords matching validation

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   // Using Constraint API
   isValid = form.checkValidity();
   // Style main message for error
-  message.textContent = 'Please fill out all fields!';
-  message.style.color = 'red';
-  message.style.borderColor = 'red';
-
+  if(!isValid) {
+    message.textContent = 'Please fill out all fields!';
+    message.style.color = 'red';
+    message.style.borderColor = 'red';
+  }
+  // Checking for password matching
+  if(password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+  } else  {
+    passwordsMatch = false;
+    message.textContent = 'Make sure passwords match.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password2El.style.borderColor = 'red';
+  }
 }
 
 function processFormData(e) {
